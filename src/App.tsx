2afe5866bcabb5e03158e0b78234eb12cde9ae3a
hadi <hadi_960@hotmail.com>
2021-10-08 23:45:25 -0500

    adds state and update the todo in the input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-import InputFeild from './components/InputFeild';
-
+import InputFeild from './components/InputFeild';   
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
+  console.log(todo)
   return (
     <div className="App">
     <span className="heading"> 2-Do</span>
-    <InputFeild /> 
+    <InputFeild todo={todo} setTodo={setTodo} /> 
 
     </div>
   );
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./styles.css";
 
-const InputFeild = () => {
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputFeild: React.FC<Props> = ({ todo, setTodo }: Props) => {
     return (
         <form className='input'>
-             <input type='input' placeholder="Enter a task" className="input__box"></input>
+             <input type='input' 
+                value={todo}
+                onChange={
+                    (e) =>setTodo(e.target.value)
+                }
+             placeholder="Enter a task" className="input__box"></input>
              <button className="input_submit" type="submit"> GO </button>
         </form>
     )
