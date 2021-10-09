9d6a049052c05eacd0a34657c4e036e7459b8455
hadi <hadi_960@hotmail.com>
2021-10-09 11:31:01 -0500

    set antoher state for todos to render in console and shows random id, todo and set false for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';   
+import { Todo } from './components/model';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
-  console.log(todo)
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if(todo) {
+      setTodos([...todos,{id:Date.now(),todo, isDone: false}])
+      setTodo("")
+    }
+  }
+
+  console.log(todos)
   return (
     <div className="App">
     <span className="heading"> 2-Do</span>
-    <InputFeild todo={todo} setTodo={setTodo} /> 
+    <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} /> 
 
     </div>
   );
